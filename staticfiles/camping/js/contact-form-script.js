0e845ae98e5e0c6adf22e3892016dbd271f1edeb
SeeDCharlie <hellocarloscastillo@gmail.com>
2021-03-04 17:41:45 -0500

    asdasd

--- staticfiles/camping/js/contact-form-script.js
+++ staticfiles/camping/js/contact-form-script.js
@@ -1,52 +1,59 @@
-$("#contactForm").validator().on("submit", function (event) {
-    if (event.isDefaultPrevented()) {
-        // handle the invalid form...
-        formError();
-        submitMSG(false, "Hay campos vacios en el formulario de contacto!");
-    } else {
-        // everything looks good!
-        event.preventDefault();
-        submitForm();
-    }
-});
 
-
-function submitForm(){
+$('.btnContReq').on('click', function (evt) {
     // Initiate Variables With Form Content
     var name = $("#name").val();
     var email = $("#email").val();
-    var msg_subject = $("#msg_subject").val();
+    var cell = $("#cel").val();
     var message = $("#message").val();
 
-
-    $.ajax({
-        type: "POST",
-        url: "php/form-process.php",
-        data: "name=" + name + "&email=" + email + "&msg_subject=" + msg_subject + "&message=" + message,
-        success : function(text){
-            if (text == "success"){
-                formSuccess();
-            } else {
-                formError();
-                submitMSG(false,text);
-            }
-        }
+    if (name == '' || cell == '' || message == ''){
+        formError();
+        showToastMsj('error', 'Llene los datos para enviar el formulario de contacto');
+    }else{
+        
+    var datos = JSON.stringify({
+        name, name,
+        cell: cell,
+        email: email,
+        message: message
     });
+    submitForm(datos);
+    }
+
+});
+
+function submitForm(datos) {
+    requestAjax('submitContact', datos);
 }
 
-function formSuccess(){
+function respuestaAjax(data) {
+    showToastMsj('Muy Bien', data.msj);
+    formSuccess();
+}
+
+function errorSucces(msj) {
+    showToastMsj('Muy Mal', msj);
+    formError();
+}
+function errorAjax() {
+    formError();
+    showToastMsj('Muy Mals', 'Compruebe su conexion a internet');
+}
+
+function formSuccess() {
     $("#contactForm")[0].reset();
-    submitMSG(true, "Message Submitted!")
+    submitMSG(true, "Mensaje Enviado!")
 }
 
-function formError(){
-    $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+function formError() {
+    $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
         $(this).removeClass();
     });
+    submitMSG(false, "Hay campos vacios en el formulario de contacto!");
 }
 
-function submitMSG(valid, msg){
-    if(valid){
+function submitMSG(valid, msg) {
+    if (valid) {
         var msgClasses = "h3 text-center tada animated text-success";
     } else {
         var msgClasses = "h3 text-center text-danger";
